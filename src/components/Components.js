abc00d37d86937001323d70841891fe3f860f522
Hector Rivera <redmagix2000@gmail.com>
2022-12-14 02:17:47 -0800

    added some detail changes

--- src/components/Components.js
+++ src/components/Components.js
@@ -1,11 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { FaJsSquare, FaPython, FaDatabase, FaCode } from "react-icons/fa";
+import { FaJsSquare, FaPython, FaDatabase, FaCode, FaEnvelope, FaPhoneSquareAlt, FaAddressBook } from "react-icons/fa";
 export const ContactInfo=()=>{
 	return(
 		<div className='pr-0 ContactInfo'>
-			<li className = 'list-group-item'>PhoneNumber: (956) 422 - 0933</li>
-			<li className = 'list-group-item'>Email:<a href="mailto:redmagix2000@gmail.com">redmagix2000@gmail.com</a></li>
-			<li className = 'list-group-item'>Address: 427 Eagle Mountain Dr, Rio Grande City, Tx</li>
+			<li className = 'list-group-item'>
+				<FaPhoneSquareAlt />
+				PhoneNumber: 
+				<span>
+					(956) 422 - 0933
+				</span>
+			</li>
+			<li className = 'list-group-item'>
+				<FaEnvelope />Email:
+				<a href="mailto:redmagix2000@gmail.com">
+					<span>
+						redmagix2000@gmail.com
+					</span>
+				</a>
+			</li>
+			<li className = 'list-group-item'>
+				<FaAddressBook />
+				<span>
+					Address: 427 Eagle Mountain Dr, Rio Grande City, Tx
+				</span>					
+			</li>
 		</div>
 	);
 };
