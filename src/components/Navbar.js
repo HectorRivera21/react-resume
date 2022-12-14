78e6688bd2f10e37054b6fa6ef7dc5f31e33b4ec
Hector Rivera <redmagix2000@gmail.com>
2022-12-14 01:56:59 -0800

    fixed some issues with span in NavBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,23 @@
-import { FaLinkedin, FaInstagramSquare, FaGithubSquare, FaTwitter } from "react-icons/fa";
+import { FaLinkedin, FaInstagramSquare, FaGithubSquare, FaTwitter ,FaFire } from "react-icons/fa";
 export const NavBar = () =>{
     return(
         <nav>
-            <a href="https://www.linkedin.com/in/hector-rivera-651b04255/">
-            <FaLinkedin />
-            <span>
-                Linkedin
-            </span>
+        
+            <a  href="https://www.linkedin.com/in/hector-rivera-651b04255/">
+                <FaLinkedin />
+                <span>
+                    Linkedin
+                </span>
             </a>|
+        
+           
             <a href="https://www.instagram.com/hector_.21/">
                 <FaInstagramSquare />
                 <span>
                     Instagram
                 </span>
             </a>|
+            
             <a href="https://github.com/HectorRivera21">
                 <FaGithubSquare />
                 <span>
@@ -24,6 +28,12 @@ export const NavBar = () =>{
                 <FaTwitter />
                 <span>
                     Twitter
+                </span>
+            </a>|
+            <a href="https://console.firebase.google.com/project/hector-react-firebase/overview">
+                <FaFire />
+                <span>
+                    FireBase
                 </span>
             </a>
         </nav>
