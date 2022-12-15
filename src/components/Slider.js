b9d5f99b12d6dfeb9c87716af5ace05d59ce4c84
HectorRivera <redmagix2000@gmail.com>
2022-12-14 18:50:11 -0600

    uniform width for all cards in Slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -5,23 +5,23 @@ export const Slider = () =>{
     return(
         <Carousel fade>
             <Carousel.Item>
-                <div className="mx-auto pr-0 p-3 col-md- card m-2 w-40">
+                <div className="card mx-auto pr-0 p-3 col-md- m-2 w-40">
                     <h2 className="card-title">Contact Info</h2>
                     <ContactInfo />
                 </div>
             </Carousel.Item>
             <Carousel.Item>
-            <div className="mx-auto pr-0 p-3 col-md- card m-2 w-25">
+            <div className="card mx-auto pr-0 p-3 col-md- m-2 w-40">
                 <Education />
             </div>
             </Carousel.Item>
             <Carousel.Item>
-                <div className="mx-auto pr-0 p-3 col-md- card m-2 w-25">
+                <div className="card mx-auto pr-0 p-3 col-md- m-2 w-40">
                     <Experience />
                 </div>
             </Carousel.Item>
             <Carousel.Item>
-                <div className="mx-auto pr-0 p-3 col-md- card m-2 w-25">
+                <div className="card mx-auto pr-0 p-3 col-md- m-2 w-40">
                     <Skills />
                 </div>
             </Carousel.Item>
