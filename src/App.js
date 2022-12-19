33d3d9954fbdd0cb770c1dd5cb28500c33e6d376
Hector Rivera <redmagix2000@gmail.com>
2022-12-19 01:30:33 -0800

    added more responsive navbar for mobile devices

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/Navbar';
 import { Slider } from './components/Slider';
-import { NavBar } from './components/Navbar';
+
 
 function App() {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,56 +1,75 @@
+import { Component } from "react";
 import { FaLinkedin, FaInstagramSquare, FaGithubSquare, FaTwitter ,FaFire } from "react-icons/fa";
 import "./Components.css";
-export const NavBar = () =>{
-    return(
-        <nav>
-            <div>
-                <ul id="navbar">
-                    <li id = "resumetitle">
+class NavBar extends Component{
+    state = {clicked: false};
+    handleClick=()=>
+    {
+        this.setState({clicked:!this.state.clicked});
+    }
+    render(){
+        return(
+            <nav>
+                <div>
+                    <div id = "resumetitle">
                         <h1 >Hector's Resume</h1>
-                    </li>
-                    <li>
-                        <a  href="https://www.linkedin.com/in/hector-rivera-651b04255/">
-                            <FaLinkedin />
-                            <span>
-                                Linkedin
-                            </span>
-                        </a>
-                    </li>
-                
-                    <li>
-                        <a href="https://www.instagram.com/hector_.21/">
-                            <FaInstagramSquare />
-                            <span>
-                                Instagram
-                            </span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/HectorRivera21">
-                            <FaGithubSquare />
-                            <span>
-                                GitHub
-                            </span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://twitter.com/hrivera_21">
-                            <FaTwitter />
-                            <span>
-                                Twitter
-                            </span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://console.firebase.google.com/project/hector-react-firebase/overview">
-                            <FaFire />
-                            <span>
-                                FireBase
-                            </span>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    );
+                    </div>
+                    <ul 
+                        id="navbar" 
+                        className={this.state.clicked ? 
+                        "#navbar active":"navbar"}
+                    >
+                        
+                        <li>
+                            <a  href="https://www.linkedin.com/in/hector-rivera-651b04255/">
+                                <FaLinkedin />
+                                <span>
+                                    Linkedin
+                                </span>
+                            </a>
+                        </li>
+                    
+                        <li>
+                            <a href="https://www.instagram.com/hector_.21/">
+                                <FaInstagramSquare />
+                                <span>
+                                    Instagram
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/HectorRivera21">
+                                <FaGithubSquare />
+                                <span>
+                                    GitHub
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/hrivera_21">
+                                <FaTwitter />
+                                <span>
+                                    Twitter
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://console.firebase.google.com/project/hector-react-firebase/overview">
+                                <FaFire />
+                                <span>
+                                    FireBase
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div id="mobile" onClick={this.handleClick}>
+                    <i id="bar" className={this.state.clicked ? 'fas fa-times':'fas fa-bars'}></i>
+                    
+                </div>
+            </nav>
+        );
+    }
 }
+
+export default NavBar;
